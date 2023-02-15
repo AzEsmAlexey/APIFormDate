504ef811e1566a1c99e7cdb8751da17c440bf580
Neto.Changes <infero-sky@yandex.ru>
2023-02-16 01:33:27 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Post form = new Post();
+
+        form.birthday.day = 01;
+        form.birthday.month = 11;
+        form.birthday.year = 1892;
+        form.name = "Эраст";
+        form.passport = "6012 №143891";
+        form.patronymic = "Цыцеринек";
+        form.phone = "8 (922) 133-14-14";
+        form.surname = "Махласифуйсович";
+        form.subscription = true;
+
+    }
+}
